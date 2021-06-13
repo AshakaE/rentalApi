086976253af2e8f799f236a999a1b390f8ac98b8
AshakaE <ashakaegerega@gmail.com>
2021-06-13 01:14:22 +0100

    Modify bookings controller

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -3,14 +3,19 @@ module Api
     class BookingsController < ApplicationController
       skip_before_action :verify_authenticity_token, only: :create
 
-      def index
-        @bookings = Booking.all.paginate(page: params[:page], per_page: 10)
-        json_response(@bookings)
-      end
+      # def index
+      #   @bookings = Booking.all.paginate(page: params[:page], per_page: 10)
+      #   json_response(@bookings)
+      # end
 
       def create
         @booking = current_user.bookings.create!(book_params)
         json_response({message: 'Successfully booked.'})
+      end
+
+      def show
+        @bookings = Booking.where(user_id: current_user)
+        json_response(@bookings)
       end
 
       private 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :bookings, only: %i[index create update destroy]
+      resources :bookings, only: %i[create show update destroy]
       resources :cars, only: %i[index create show]
     end    
   end
