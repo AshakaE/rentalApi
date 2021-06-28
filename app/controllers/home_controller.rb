df8886ae893775af15a0f4a4c053e5ccef9d8f33
AshakaE <ashakaegerega@gmail.com>
2021-06-28 09:40:07 +0100

    Remove root route

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  skip_before_action :verify_authenticity_token, :authorize_request
-  def index
-    json_response('Welcome to Carenter')
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
   namespace :api do
     namespace :v1 do
       resources :bookings, only: %i[index create show update destroy]
